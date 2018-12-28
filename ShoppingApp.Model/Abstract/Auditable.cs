04bf837c62b6f4eceb513a4528b4720342c23a10
hungbn <hungdo99tt@gmail.com>
2018-12-28 23:17:22 +0700

    add domain ef model

---
+++ ShoppingApp.Model/Abstract/Auditable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ShoppingApp.Model.Abstract
+{
+  public abstract class Auditable : IAuditable
+  {
+   public DateTime? CreatedDate { set; get; }
+
+   [MaxLength(256)]
+   public string CreatedBy { set; get; }
+   public DateTime? UpdatedDate { set; get; }
+
+   [MaxLength(256)]
+   public string UpdatedBy { set; get; }
+   [MaxLength(256)]
+   public string MetaKeyword { get; set; }
+   [MaxLength(256)]
+   public string MetaDescription { get; set; }
+   public bool Status { get; set; }
+  }
+}
---
+++ ShoppingApp.Model/Abstract/IAuditable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingApp.Model.Abstract
+{
+  public interface IAuditable
+  {
+
+    DateTime? CreatedDate { set; get; }
+
+    
+    string CreatedBy { set; get; }
+
+    DateTime? UpdatedDate { set; get; }
+
+    [MaxLength(256)]
+    string UpdatedBy { set; get; }
+
+    string MetaKeyword { get; set; }
+    string MetaDescription { get; set; }
+
+    bool Status { get; set; }
+  }
+}
