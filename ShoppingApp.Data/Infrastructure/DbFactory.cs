10ea3a3b338aedec60d7fede5903e46fa8a0e38c
hungbn <hungdo99tt@gmail.com>
2018-12-31 00:03:19 +0700

    repository and migration

---
+++ ShoppingApp.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingApp.Data.Infrastructure
+{
+  public class DbFactory : Disposable, IDbFactory
+  {
+    ShoppingAppDbContext dbContext;
+    public ShoppingAppDbContext Init()
+    {
+      return dbContext ?? (dbContext = new ShoppingAppDbContext());
+    }
+    protected override void DisposeCore()
+    {
+      if(dbContext != null)
+      {
+        dbContext.Dispose();
+      }
+    }
+  }
+}
