d8f383e15ca9b297a5cf1cad4ece10d58a2bd01f
hungbn <hungdo99tt@gmail.com>
2019-01-06 21:34:35 +0700

    add unit test for repository and service

--- ShoppingApp.Data/Infrastructure/IRepository.cs
+++ ShoppingApp.Data/Infrastructure/IRepository.cs
@@ -10,20 +10,20 @@ namespace ShoppingApp.Data.Infrastructure
   public interface IRepository<T> where T : class
   {
     //marks an entity as new
-    void Add(T entity);
+    T Add(T entity);
     //marks an entity as modified
     void Update(T entity);
     //marks an entity to be removed
-    void Delete(T entity);
-    void Delete(int id);
+    T Delete(T entity);
+    T Delete(int id);
     //delete multiple records
     void DeleteMulti(Expression<Func<T, bool>> where);
     //get an entity by int id
     T GetSingleById(int id);
     T GetSingleByCondition(Expression<Func<T,bool>> expression, string[] includes = null);
-    IQueryable<T> GetAll(string[] includes = null);
-    IQueryable<T> GetMulti(Expression<Func<T,bool>> predicate, string[] includes = null);
-    IQueryable<T> GetMultiPaging(Expression<Func<T, bool>> filter,out int total,int index=0,int size=50,string[]includes = null);
+    IEnumerable<T> GetAll(string[] includes = null);
+    IEnumerable<T> GetMulti(Expression<Func<T,bool>> predicate, string[] includes = null);
+    IEnumerable<T> GetMultiPaging(Expression<Func<T, bool>> filter,out int total,int index=0,int size=50,string[]includes = null);
     int Count(Expression<Func<T, bool>> where);
     bool CheckContains(Expression<Func<T, bool>> predicate);
 
---
+++ ShoppingApp.UnitTest/RepositoryTest/PostCategoryRepositoryTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using ShoppingApp.Data.Infrastructure;
+using ShoppingApp.Data.Repositories;
+using ShoppingApp.Model.Models;
+
+namespace ShoppingApp.UnitTest.RepositoryTest
+{
+  [TestClass]
+  public class PostCategoryRepositoryTest
+  {
+    IDbFactory dbFactory;
+    IPostCategoryRepository objRepository;
+    IUnitOfWork unitOfWork;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+      dbFactory = new DbFactory();
+      objRepository = new PostCategoryRepository(dbFactory);
+      unitOfWork = new UnitOfWork(dbFactory);
+    }
+    [TestMethod]
+    public void PostCategory_Repository_GetAll()
+    {
+      var list = objRepository.GetAll().ToList();
+      Assert.AreEqual(3, list.Count);
+    }
+    [TestMethod]
+    public void PostCategory_Repository_Create()
+    {
+      PostCategory category = new PostCategory();
+      category.Name = "test category";
+      category.Alias = "test - category";
+      category.Status =true;
+      var result = objRepository.Add(category);
+      unitOfWork.Commit();
+      Assert.IsNotNull(result);
+      Assert.AreEqual(2, result.ID);
+    }
+  }
+}
