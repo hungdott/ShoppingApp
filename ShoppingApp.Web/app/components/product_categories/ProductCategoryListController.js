b823e49fa1d34a18c2c0b27c440d08dd2da4f254
hungbn <hungdo99tt@gmail.com>
2019-01-11 00:05:16 +0700

    notification service

--- ShoppingApp.Web/app/components/product_categories/ProductCategoryListController.js
+++ ShoppingApp.Web/app/components/product_categories/ProductCategoryListController.js
@@ -1,8 +1,8 @@
 ﻿/// <reference path="D:\asp.net\AppShopASP\Git\ShoppingApp.Web\Assets/admin/libs/angular/angular.js" />
 (function (app) {
     app.controller('productCategoryListController', productCategoryListController)
-    productCategoryListController.$inject = ['$scope','apiService']
-    function productCategoryListController($scope, apiService) {
+    productCategoryListController.$inject = ['$scope','apiService','notificationService']
+    function productCategoryListController($scope, apiService, notificationService) {
         $scope.productCategories = []
         $scope.page = 0
         $scope.pagesCount = 0
@@ -25,6 +25,17 @@
                 }
             }
             apiService.get('/api/productcategory/getall', config, function (result) {
+                if (result.data.TotalCount == 0) {
+                    notificationService.displayWarning('Không có bản ghi nào được tìm thấy')
+                }
+                else {
+                    if ($scope.keyword.length == 0) {
+                        notificationService.displayWarning('nhập vào ô tìm kiếm')
+                    }
+                    else
+                        notificationService.displaySuccess('Tìm thấy: ' + result.data.TotalCount+' bản ghi')
+                }
+                
                 $scope.productCategories = result.data.Item
                 $scope.page = result.data.Page
                 $scope.pagesCount = result.data.TotalPages
---
+++ ShoppingApp.Web/app/shared/service/notificationService.js
@@ -0,0 +1,43 @@
+﻿/// <reference path="D:\asp.net\AppShopASP\Git\ShoppingApp.Web\Assets/admin/libs/toastr/toastr.js" />
+/// <reference path="D:\asp.net\AppShopASP\Git\ShoppingApp.Web\Assets/admin/libs/angular/angular.js" />
+(function (app) {
+    app.factory('notificationService', notificationService)
+    function notificationService() {
+        toastr.options = {
+            "closeButton": true,
+            "debug": false,
+            "positionClass": "toast-top-right",
+            "onclick": null,
+            "fadeIn": 300,
+            "fadeOut": 1000,
+            "timeOut": 4000,
+            "extendedTimeOut": 1000,
+            "showEasing": "swing",
+            "hideEasing": "linear",
+        }
+        
+        function displaySuccess(message) {
+            toastr["success"](message)
+        }
+        function displayError(message) {
+            if (Array.isArray(message)) {
+                message.each(function (err) {
+                    toastr["error"](err)
+                })
+            }
+            toastr["error"](message)
+        }
+        function displayWarning(message) {
+            toastr["warning"](message)
+        }
+        function displayInfo(message) {
+            toastr["info"](message)
+        }
+        return {
+            displaySuccess: displaySuccess,
+            displayError: displayError,
+            displayWarning: displayWarning,
+            displayInfo: displayInfo,
+        }
+    }
+})(angular.module('shoppingapp.common'))
