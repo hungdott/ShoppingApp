d55b88b322fe0efb6652ac92c0236ff53dbfba03
hungbn <hungdo99tt@gmail.com>
2019-01-10 20:04:07 +0700

    filter status and format data

---
+++ ShoppingApp.Web/app/shared/filers/statusFilter.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="D:\asp.net\AppShopASP\Git\ShoppingApp.Web\Assets/admin/libs/angular/angular.js" />
+(function (app) {
+    app.filter('statusFilter', function () {
+        return function (input) {
+            if (input == true) {
+                return 'kích hoạt'
+            }
+            else {
+                return 'khóa'
+            }
+        }
+    })
+})(angular.module('shoppingapp.common'))
