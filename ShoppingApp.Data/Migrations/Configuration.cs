0c20024509186337b7acd0d87c5f8ebfd006502e
hungbn <hungdo99tt@gmail.com>
2019-02-12 23:58:53 +0700

    binding slide and product to index

--- ShoppingApp.Data/Migrations/Configuration.cs
+++ ShoppingApp.Data/Migrations/Configuration.cs
@@ -20,6 +20,10 @@
     protected override void Seed(ShoppingApp.Data.ShoppingAppDbContext context)
     {
       CreateProductCategorySample(context);
+      CreateSlide(context);    
+    }
+    private void CreateUser(ShoppingAppDbContext context)
+    {
       //var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ShoppingAppDbContext()));
       //var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new ShoppingAppDbContext()));
       //var user = new ApplicationUser()
@@ -62,6 +66,35 @@
       if (context.Footers.Count(x => x.ID == CommonConstants.DefaultFooterId)==0)
       {
         string content = "";
+      }
+    }
+    private void CreateSlide(ShoppingAppDbContext context)
+    {
+      if (context.Slides.Count() == 0)
+      {
+        List<Slide> listSlide = new List<Slide>()
+        {
+          new Slide() {
+            Name ="slide 1",
+            DisplayOrder = 1,
+            Status =true,
+            Url ="#",
+            Image ="/Assets/client/images/bag.jpg",
+          
+            Content = @"<h2>FLAT 50% 0FF</h2><label>FOR ALL PURCHASE <b>VALUE</b></label><p>Lorem ipsum dolorsit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et </p><span class=""on-get"">GET NOW</span>"
+          },
+          new Slide() {
+            Name ="slide 2",
+            DisplayOrder = 2,
+            Status =true,
+            Url ="#",
+            Image ="/Assets/client/images/bag1.jpg",
+         
+            Content= @"<h2>FLAT 50% 0FF</h2><label>FOR ALL PURCHASE <b>VALUE</b></label><p>Lorem ipsum doloritamet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et </p><span class=""on-get"">GET NOW</span>"
+          }
+        };
+        context.Slides.AddRange(listSlide);
+        context.SaveChanges();
       }
     }
   }
