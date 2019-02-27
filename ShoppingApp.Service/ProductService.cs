9cec16ae3b9674d28de6e0da52889328b732686d
Admin <Admin@DESKTOP-PGUCCN0>
2019-02-27 10:26:03 +0700

    auto complete search with jqeury-ui

--- ShoppingApp.Service/ProductService.cs
+++ ShoppingApp.Service/ProductService.cs
@@ -22,6 +22,9 @@ namespace ShoppingApp.Service
     IEnumerable<Product> GetLastest(int top);
     IEnumerable<Product> GetHotProduct(int top);
     IEnumerable<Product> GetListProductByCategoryIdPaging(int categoryId, int page, int pageSize, string sort, out int totalRow);
+    IEnumerable<Product> Search(string keyword, int page, int pageSize, string sort, out int totalRow);
+
+    IEnumerable<string> GetListProductByName(string name);
 
     Product GetById(int id);
 
@@ -141,6 +144,34 @@ namespace ShoppingApp.Service
       switch (sort)
       {
         
+        case "popular":
+          query = query.OrderByDescending(x => x.ViewCount);
+          break;
+        case "discount":
+          query = query.OrderByDescending(x => x.PromotionPrice.HasValue);
+          break;
+        case "price":
+          query = query.OrderBy(x => x.Price);
+          break;
+        default:
+          query = query.OrderByDescending(x => x.CreatedDate);
+          break;
+      }
+      totalRow = query.Count();
+      return query.Skip((page - 1) * pageSize).Take(pageSize);
+    }
+
+    public IEnumerable<string> GetListProductByName(string name)
+    {
+      return _productRepository.GetMulti(x=>x.Status==true && x.Name.Contains(name)).Select(x=>x.Name);  
+    }
+
+    public IEnumerable<Product> Search(string keyword, int page, int pageSize, string sort, out int totalRow)
+    {
+      var query = _productRepository.GetMulti(x => x.Status == true && x.Name.Contains(keyword));
+      switch (sort)
+      {
+
         case "popular":
           query = query.OrderByDescending(x => x.ViewCount);
           break;
