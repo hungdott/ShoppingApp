ea57667af5de2cf590688312a9b438e3e689cd36
Admin <Admin@DESKTOP-PGUCCN0>
2019-03-03 15:03:42 +0700

    add output cache for website

--- ShoppingApp.Web/Controllers/HomeController.cs
+++ ShoppingApp.Web/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace ShoppingApp.Web.Controllers
       _productService = productService;
       _commonService = commonService;
     }
-
+    [OutputCache(Duration = 60, Location = System.Web.UI.OutputCacheLocation.Server)]
     public ActionResult Index()
     {
       var slideModel = _commonService.GetSlides();
@@ -39,21 +39,8 @@ namespace ShoppingApp.Web.Controllers
       return View(homeViewModel);
     }
 
-    public ActionResult About()
-    {
-      ViewBag.Message = "Your application description page.";
-
-      return View();
-    }
-
-    public ActionResult Contact()
-    {
-      ViewBag.Message = "Your contact page.";
-
-      return View();
-    }
-
     [ChildActionOnly]
+    [OutputCache(Duration =3600)]
     public ActionResult Footer()
     {
       var footerModel = _commonService.GetFooter();
@@ -66,6 +53,7 @@ namespace ShoppingApp.Web.Controllers
       return PartialView();
     }
     [ChildActionOnly]
+    [OutputCache(Duration = 3600)]
     public ActionResult Category()
     {
       var model = _productCategoryService.GetAll();
